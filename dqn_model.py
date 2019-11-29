4a2dc3e15e018df5eeab842b57a2f1326b8077bc
gordon <gordonzhang81@gmail.com>
2019-11-29 15:26:05 -0500

    dqn

---
+++ dqn_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DQN(nn.Module):
+    """Initialize a deep Q-learning network
+
+    Hints:
+    -----
+        Original paper for DQN
+    https://storage.googleapis.com/deepmind-data/assets/papers/DeepMindNature14236Paper.pdf
+
+    This is just a hint. You can build your own structure.
+    """
+    def __init__(self, input_size, output_size):
+        """
+        You can add additional arguments as you need.
+        In the constructor we instantiate modules and assign them as
+        member variables.
+        """
+        super(DQN, self).__init__()
+        ###########################
+        # YOUR IMPLEMENTATION HERE #
+        self.fc1 = nn.Linear(input_size, 100)
+        self.fc2 = nn.Linear(100, 100)
+        self.fc3 = nn.Linear(100, 100)
+
+        self.bn1 = nn.BatchNorm1d(100)
+        self.bn2 = nn.BatchNorm1d(100)
+        self.bn3 = nn.BatchNorm1d(100)
+
+        self.head = nn.Linear(100, output_size)
+
+
+    def forward(self, x):
+        """
+        In the forward function we accept a Tensor of input data and we must return
+        a Tensor of output data. We can use Modules defined in the constructor as
+        well as arbitrary operators on Tensors.
+        """
+        ###########################
+        # YOUR IMPLEMENTATION HERE #
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn2(self.fc2(x)))
+        x = F.relu(self.bn3(self.fc3(x)))
+        ###########################
+        return self.head(x)
